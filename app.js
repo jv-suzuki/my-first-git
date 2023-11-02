778ddbdffa34108740cf5e030c6c0a3ae8b832a5
Masaki Suzuki <masuzuki201405@gmail.com>
2023-11-03 08:33:57 +0900

    .gitignoreを再度追加

--- app.js
+++
@@ -1,6 +0,0 @@
-$("#addTodo").click(function(){
-    const inputTodo = $("input").val();
-    $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
-
-    $("input").val("");
-})
